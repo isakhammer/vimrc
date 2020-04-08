7c7c4a2eeb650a1e13ba570c2ec85b0e8d539682
isakh <isakhammer@gmail.com>
2020-04-08 21:32:19 +0200

    Removed bash stuff in install vimrc.

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -27,19 +27,3 @@ echo ""
 echo "====="
 sleep 0.5
 echo "Finished writing"
-
-
-echo "Making backup of your ~/.bashrc at: ~/.bashrc_backup_"$NOW
-cp ~/.bashrc ~/.bashrc_backup_$NOW
-echo "Writing into your ~/.bashrc."
-echo "Starts writing"
-sleep 0.5
-echo "====="
-echo ""
-echo 'source ~/.vim_runtime/custom/my_bash_configs.sh' |& tee -a ~/.bashrc
-echo ""
-echo "====="
-sleep 0.5
-echo "Finished writing"
-
-echo "Installed the Ultimate Isak Vim configuration successfully! Enjoy :-)"
