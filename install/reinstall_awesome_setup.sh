fa207f1372ffea2c412ceaa6b40e0138d187fa7f
isakh <isakhammer@gmail.com>
2020-04-09 00:52:41 +0200

    Fixing reinstall bug.

--- install/reinstall_awesome_setup.sh
+++ install/reinstall_awesome_setup.sh
@@ -4,6 +4,7 @@ echo "Making backup of your ~/.bashrc at: ~/.bashrc_backup_"$NOW
 cp ~/.bashrc ~/.bashrc_backup_$NOW
 vim -s uninstall_bashrc.txt ~/.bashrc
 
+cd ~
 rm -Rf ~/.vim_runtime
-source ~/.vim_runtime/install/install_awesome_vimrc.sh
-source ~/.vim_runtime/install/install_awesome_bashrc.sh
+git clone --depth=1 https://github.com/isakhammer/vimrc.git ~/.vim_runtime
+source ~/.vim_runtime/install/install_awesome_setup.sh
