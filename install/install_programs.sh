7c05dea66f45e39da07cf906f1a876e0e5b7198b
Colin <isakhammer@gmail.com>
2020-04-11 00:04:33 +0200

    Essentials of programs.

--- install/install_programs.sh
+++ install/install_programs.sh
@@ -1,6 +1,10 @@
 sudo apt install cowsay
+sudo apt install freecad
 sudo apt install snapd
 snap install googler
+snap install discord
+snap install spotify
+snap install htop
 sudo apt-get --assume-yes install zathura
 sudo apt-get --assume-yes install xdotool
 sudo apt-get --assume-yes install latexmk
