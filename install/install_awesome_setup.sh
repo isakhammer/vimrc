3a19919c0ea50fb609076729c796334fe7b44dba
Colin <isakhammer@gmail.com>
2020-04-11 11:46:40 +0200

    Removed bash related stuff.

--- install/install_awesome_setup.sh
+++
@@ -1,2 +0,0 @@
-source ~/.vim_runtime/install/install_awesome_vimrc.sh
-source ~/.vim_runtime/install/install_awesome_bashrc.sh
---
+++ install/reinstall_awesome_vimrc.sh
@@ -0,0 +1,4 @@
+cd ~
+rm -Rf ~/.vim_runtime
+git clone --depth=1 https://github.com/isakhammer/vimrc.git ~/.vim_runtime
+source ~/.vim_runtime/install/install_awesome_vimrc.sh
